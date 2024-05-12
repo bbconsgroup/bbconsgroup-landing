8d15fef6305b6aaf39bc4c6085f596fdccfd78dd
Sundet <aidar@magnum.kz>
2024-05-12 18:24:00 +0500

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./templates/Home";
+import About from "./templates/About";
+import Contact from "./templates/Contact";
+import Navbar from "./Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/templates/Contact.js
@@ -0,0 +1,19 @@
+export default function Contact() {
+  return (
+    <div>
+      <h1>
+        <center>
+          <p>Contact Me.</p>
+          <a
+            href="https://github.com/soondet"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sundet Aidar (GitHub) [@soondet]
+          </a>
+        </center>
+      </h1>
+    </div>
+  );
+}
+
